fc7c31c6989a487b1a469d0fc65935db31191c03
cozy <dio7890@gmail.com>
2023-10-23 13:31:58 +0900

    本番用データパス変更

--- src-tauri/src/config.rs
+++ src-tauri/src/config.rs
@@ -1,3 +1,3 @@
 pub const SPREADSHEET_ID: &str = "1CLB6stHi6SdRfjwmuSIhzYUCk4kZXuiv5xXN_nr4JzE";
 pub const DATA_REFRESH_INTERVAL: u64 = 60;
-pub const DATA_FILE_PATH: &str = "../../";
+pub const DATA_FILE_PATH: &str = "./";
